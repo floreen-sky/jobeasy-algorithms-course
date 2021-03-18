5c697d752b6fa382ae2b44def2b80ad7c04ee12f
floreen-sky <floreen.sky@gmail.com>
2021-03-18 16:32:04 -0400

    Home Work 7 - Algorithms Lesson 5

---
+++ hw_7_alg5.py
@@ -0,0 +1,22 @@
+# 1. When given a list, the program should return a list of all the elements that are below the arithmetical mean
+# of the original list.
+#
+# list1 = list(map(int, input("Enter the elements of the list separated by space : ").strip().split()))
+# a_mean = sum(list1) // len(list1)
+# list2 = []
+# for i in list1:
+#     if i < a_mean:
+#         list2.append(i)
+# print(f'The elements from the entered list that are bellow the arithmentical mean are: {list2}')
+
+
+
+# # 2. When given a list of elements find the two lowest elements. They can be equal to each other or different.
+#
+# list3 = list(map(int, input("Enter the elements of the list separated by space : ").strip().split()))
+# list4 = list(set(list3))
+# list4.sort()
+# print(list4[:2])
+
+
+
